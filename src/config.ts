2c10ae597848912eabb6f3366a67b28c44ecd454
Damsonite <damsonite321@outlook.com>
2024-08-06 18:42:30 -0500

    Modified Header & Footer info and site name

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "damsonite.web";
+export const SITE_TITLE = "damsonite.dev";
 export const SITE_DESCRIPTION = "David Mora personal website";
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
